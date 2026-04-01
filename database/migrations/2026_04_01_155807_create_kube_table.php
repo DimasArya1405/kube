443db85cbdf43e4509001645ffcf7b38ee769884
AnayAilirpa <dianaaprilia745@gmail.com>
2026-04-01 23:01:28 +0700

    add tabel kube

---
+++ database/migrations/2026_04_01_155807_create_kube_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kube', function (Blueprint $table) {
+            $table->id('id_kube');
+            $table->string('nama_kube', 100);
+            $table->unsignedBigInteger('id_desa_kelurahan');
+            $table->unsignedBigInteger('id_cluster');
+            $table->date('tanggal_terbentuk')->nullable(); // Boleh kosong di awal
+            $table->string('status', 15)->default('Menunggu'); // Default status
+            $table->text('keterangan');
+
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+
+            $table->foreign('id_desa_kelurahan')
+                ->references('id_desa_kelurahan')
+                ->on('desa_kelurahan')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->foreign('id_cluster')
+                ->references('id_cluster')
+                ->on('cluster')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kube');
+    }
+};
