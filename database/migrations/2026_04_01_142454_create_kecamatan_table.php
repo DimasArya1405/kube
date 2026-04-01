74e94100546cf2108068dcc668875164e2816625
AnayAilirpa <dianaaprilia745@gmail.com>
2026-04-01 23:03:08 +0700

    update tabel kecamatan

--- database/migrations/2026_04_01_142454_create_kecamatan_table.php
+++ database/migrations/2026_04_01_142454_create_kecamatan_table.php
@@ -14,9 +14,8 @@ return new class extends Migration {
         Schema::create('kecamatan', function (Blueprint $table) {
             $table->id('id_kecamatan');
             $table->string('nama_kecamatan', 100);
-            
-            // Fix: Ganti timestamps() jadi ini biar otomatis ngisi jam 
-            // pas lu pakai DB::table()->insert() di bawah
+
+
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
