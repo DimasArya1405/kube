4763ed5cb2ed93facf62e1666a202ad7a130c40b
DimasArya1405 <165295750+DimasArya1405@users.noreply.github.com>
2026-04-04 11:53:34 +0700

    Instalasi Flowbite dan membuat template awal tabel, button, dan modal

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
-        tailwindcss(),
+        // Di dalam vite.config.js
+        tailwindcss({
+            config: "./tailwind.config.js", // Tambahkan baris ini jika ingin pakai cara lama
+        }),
     ],
     server: {
         watch: {
-            ignored: ['**/storage/framework/views/**'],
+            ignored: ["**/storage/framework/views/**"],
         },
     },
 });
