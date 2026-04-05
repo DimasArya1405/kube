c05919c618e310fb97d3043f8befd657da4df815
FerinaS <Ferinasheren@gmail.com>
2026-04-05 16:28:13 +0700

    perkembangan usaha

--- database/migrations/2026_04_02_094427_create_data_perkembangan_usaha_table.php
+++ database/migrations/2026_04_02_094427_create_data_perkembangan_usaha_table.php
@@ -6,49 +6,38 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('data_perkembangan_usaha', function (Blueprint $table) {
-            $table->increments('id_hasil_kunjungan'); // INT(9) PK Auto Increment
+            $table->increments('id_perkembangan');
 
-            $table->unsignedInteger('id_kunjungan'); // FK ke tabel pendamping
-            $table->unsignedInteger('id_kube'); // FK ke tabel kube
+            $table->unsignedInteger('id_laporan');
 
-            $table->decimal('omset_sebelum', 12, 2)->nullable(); // Omset sebelum pendampingan
-            $table->decimal('omset_sesudah', 12, 2)->nullable(); // Omset setelah pendampingan
+            $table->unsignedTinyInteger('periode_bulan'); // INT 2
+            $table->unsignedSmallInteger('periode_tahun'); // INT 4
 
-            $table->unsignedTinyInteger('jumlah_tenaga_kerja')->nullable(); // Jumlah tenaga kerja
+            $table->decimal('omset_pendapatan', 12, 2)->nullable();
+
+            $table->unsignedSmallInteger('jumlah_tenaga_kerja')->nullable();
 
             $table->enum('perkembangan_usaha', ['Meningkat', 'Tetap', 'Menurun'])->nullable();
             $table->enum('tingkat_kemandirian', ['Rendah', 'Sedang', 'Tinggi'])->nullable();
 
-            $table->text('hasil_evaluasi')->nullable(); // Ringkasan evaluasi
-            $table->text('rekomendasi')->nullable(); // Rekomendasi pendamping
+            $table->text('hasil_evaluasi')->nullable();
+            $table->text('rekomendasi')->nullable();
 
             $table->enum('status_hasil', ['Tercapai', 'Belum Tercapai'])->nullable();
 
-            $table->timestamp('created_at')->useCurrent(); // Waktu input
-            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate(); // Waktu update
+            $table->timestamps();
 
-            // // Foreign Key
-            // $table->foreign('id_kunjungan')
-            //       ->references('id')
-            //       ->on('pendamping')
-            //       ->onDelete('cascade');
-
-            // $table->foreign('id_kube')
-            //       ->references('id')
-            //       ->on('kube')
-            //       ->onDelete('cascade');
+            // Foreign Key
+            $table->foreign('id_laporan')
+                  ->references('id')
+                  ->on('laporan_keuangan') // sesuaikan dengan nama tabel kamu
+                  ->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('data_perkembangan_usaha');
