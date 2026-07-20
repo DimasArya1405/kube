04c3e62a58f6edca67a2e945f03f1ee2d45e0741
Fahranni <ameladellia815@gmail.com>
2026-07-20 20:06:42 +0700

    perbaikan

--- database/migrations/2026_04_02_053703_create_bantuan_kolaborasi_table.php
+++ database/migrations/2026_04_02_053703_create_bantuan_kolaborasi_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->unsignedBigInteger('id_mitra');
             $table->foreign('id_mitra')->references('id_mitra')->on('mitra')->onDelete('cascade');
             
-            $table->unsignedBigInteger('id_kube');
+            $table->string('id_kube');
             // $table->foreign('id_kube')->references('id_kube')->on('kube')->onDelete('cascade');
 
             $table->string('jenis_bantuan', 100);
